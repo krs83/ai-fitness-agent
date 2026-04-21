d6984c0d082b4def67d41006be9759fd6320d6ab
krs83 <krs83@yandex.ru>
2026-04-21 15:51:52 +0300

    refactor: add history converstion

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Assistant:
         self.api_key = os.getenv("API_KEY")
         self.instructions = instructions
         self.previous_response_id_map = {}
+        self.history = []
         self.model = f"gpt://{self.folder_id}/qwen3-235b-a22b-fp8/latest"
         self.client = OpenAI(
             base_url = "https://ai.api.cloud.yandex.net/v1",
@@ -25,7 +26,7 @@ class Assistant:
         res = self.client.responses.create(
             model = self.model,
             store = True,
-            previous_response_id = previous_response_id,
+            # previous_response_id = previous_response_id,
             instructions = self.instructions,
             max_output_tokens = 100,
             input = input
@@ -33,6 +34,8 @@ class Assistant:
 
         # Записать ID последнего ответа модели в словаре
         self.previous_response_id_map[session_id] = res.id
+        self.history.append(f"Вопрос: {input}\n Ответ: {res.output_text}")
+        print(f"ИСТОРИЯ ПЕРЕПИСКИ:\n {self.history}")
         return res.output_text
 
 wrong_promt = "Извините, но я не обсуждаю другие темы кроме спорта"
@@ -41,4 +44,6 @@ instructions = ("Ты — опытный фитнес-тренер, задача
 
 fitness_agent = Assistant(instructions)
 print(fitness_agent(input="Привет, хочу тренироваться с гирями 16 и 24 кг"))
+print(fitness_agent(input="мой уровень средний. 16 кг для меня не очень тяжелая гиря"))
+
 
